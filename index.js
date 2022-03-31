8a4757e6a2017410ac5b25f7df7859e089d3e827
Morgan J <40442819+MorganEJLA@users.noreply.github.com>
2022-03-31 17:49:04 -0400

    added fetch request from api to get two cards

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
+let deckId
 
-
-function handleClick() {
-    fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
+function handleClick(){
+    fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/?deck_count=1")
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => {console.log(data)
+        deckId = data.deck_id
+     })
 }
 
 
 
-
 document.getElementById("new-deck").addEventListener("click", handleClick)
+document.getElementById("draw-card").addEventListener("click", () =>{
+    console.log(deckId);
+    fetch(`https://deckofcardsapi.com/api/deck/new/draw/?count=2`)
+        .then(res =>res.json())
+        .then(data => {
+            console.log((data))
+        })
+
+})
 
 
 // setTimeout(function(){
@@ -17,7 +27,3 @@ document.getElementById("new-deck").addEventListener("click", handleClick)
 // }, 2000)
 
 
-function callback(){
-    console.log("i finally ran")
-}
-setTimeout(callback, 2000)
