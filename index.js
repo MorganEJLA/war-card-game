e396ddcc77570b3a01f42c69209566b2e3a6d261
Morgan J <40442819+MorganEJLA@users.noreply.github.com>
2022-04-01 19:46:32 -0400

    added styling to the gameboard, styled buttons

--- index.js
+++ index.js
@@ -16,7 +16,10 @@ document.getElementById("draw-card").addEventListener("click", () =>{
     fetch(`https://deckofcardsapi.com/api/deck/new/draw/?count=2`)
         .then(res =>res.json())
         .then(data => {
-            console.log((data))
+            console.log(data.cards)
+            document.getElementById("cards").innerHTML = 
+            `<img src = ${data.cards[0].image} />
+             <img src = ${data.cards[1].image} />`
         })
 
 })
