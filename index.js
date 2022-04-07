74949ecbda65db5249527db6cc9307ce60f698d0
Morgan J <40442819+MorganEJLA@users.noreply.github.com>
2022-04-06 17:30:00 -0700

    added text to UI

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ let deckId
 const cardsContainer = document.getElementById("cards");
 const newDeck = document.getElementById("new-deck");
 const drawCard = document.getElementById("draw-card");
-
+const header = document.getElementById("header");
+const remainingText = document.getElementById("remaining");
+const computerScoreEl = document.getElementById("computer-score");
+const yourScoreEl = document.getElementById("your-score");
 function handleClick(){
-    fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/?deck_count=1")
+    fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
+        
         .then(res => res.json())
         .then(data => {console.log(data)
+        remainingText.textContent = `Remaining cards ${data.remaining}`
         deckId = data.deck_id
         console.log(deckId)
      })
@@ -16,34 +21,40 @@ function handleClick(){
 
 newDeck.addEventListener("click", handleClick)
 drawCard.addEventListener("click", () =>{
-    fetch(`https://deckofcardsapi.com/api/deck/new/draw/?count=2`)
+    fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`)
         .then(res =>res.json())
         .then(data => {
+            remainingText.textContent = `Remaining cards ${data.remaining}`
             cardsContainer.children[0].innerHTML = 
             `<img src = ${data.cards[0].image} class = "card" />`
             cardsContainer.children[1].innerHTML = 
              `<img src = ${data.cards[1].image} class = "card" />`
             const winnerText = determineCardWinner(data.cards[0], data.cards[1])
-            console.log(winnerText)
+            header.textContent = winnerText;
+
+            if (data.remaining === 0){
+                drawCard.disabled = true;
+            }
 
         })
 
 })
 
 function determineCardWinner(card1, card2){
-    const valueOptions = ["2","3","4","5","6","7","8","9","10", "Jack", 
-    "Queen", "King", "Ace"]
-    const card1ValueIndex = valueOptions.indexOf(card1.value);
-    const card2ValueIndex = valueOptions.indexOf(card2.value);
+    const valueOptions = ["2","3","4","5","6","7","8","9","10","JACK", 
+    "QUEEN", "KING", "ACE"]
+    const card1ValueIndex = valueOptions.indexOf(card1.value)
+    const card2ValueIndex = valueOptions.indexOf(card2.value)
     // console.log("card1: ", card1ValueIndex);
     // console.log("card2: ", card2ValueIndex);
 
     if (card1ValueIndex > card2ValueIndex){
-        return "player 1 wins!"
+        return "Card 1 leads"
     }else if (card1ValueIndex < card2ValueIndex){
-        return "player 2 wins!"
+        return "Card 2 leads!"
     }else{
-        return "Its a tie"
+        return "War!"
+    // messageEl.textContent = message;
     }
 }
 
